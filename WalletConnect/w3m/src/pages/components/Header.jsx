33fcad3fcf1820cf50405306137c459da99e7461
Nima Nikravan <nima.nikravan@ext.soprasteria.com>
2024-03-08 08:16:12 +0100

    Restored Chakra package

---
+++ WalletConnect/w3m/src/pages/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// Importing the CSS file
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo">DeFi LAb</div>
+            <div className="navigation">
+                <button className="nav-button">Button 1</button>
+                <button className="nav-button">Button 2</button>
+                <div className="hamburger-menu">☰</div>
+            </div>
+        </div>
+    );
+};git
+
+export default Header;
